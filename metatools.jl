eadf5f2305931ca14c91d7a45e1845bb6e439453
Daniel O'Malley <omalled@lanl.gov>
2015-03-12 17:54:32 -0600

    Now can search for symbols in an expr

--- metatools.jl
+++ metatools.jl
@@ -16,4 +16,24 @@ function replacesymbol!(haystack::Expr, needle::Symbol, replacement)
 	return haystack
 end
 
+function in(needle, haystack::Expr)
+	if needle == haystack.head
+		return true
+	elseif typeof(haystack.head) == Expr
+		if in(needle, haystack.head)
+			return true
+		end
+	end
+	for i = 1:length(haystack.args)
+		if needle == haystack.args[i]
+			return true
+		elseif typeof(haystack.args[i]) == Expr
+			if in(needle, haystack.args[i])
+				return true
+			end
+		end
+	end
+	return false
+end
+
 end
