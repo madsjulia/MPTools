89c8aa0c33af8bf47375053d030408e110c3e04b
Daniel O'Malley <omalled@lanl.gov>
2015-08-04 16:39:07 -0600

    nows fills in exprs with vals from dicts, and gets symbols from exprs

--- src/MPTools.jl
+++ src/MPTools.jl
@@ -1,5 +1,41 @@
 module MPTools
 
+function getsymbols(haystack::Expr)
+	symbols = Set{Symbol}()
+	if typeof(haystack.head) == Expr
+		merge!(symbols, getsymbols(haystack.head))
+	elseif typeof(haystack.head) == Symbol
+		push!(symbols, haystack.head)
+	end
+	for i = 1:length(haystack.args)
+		if typeof(haystack.args[i]) == Expr
+			merge!(symbols, getsymbols(haystack.args[i]))
+		elseif typeof(haystack.args[i]) == Symbol
+			push!(symbols, haystack.args[i])
+		end
+	end
+	return symbols
+end
+
+function populateexpression!(haystack::Expr, vals::Dict)
+	if typeof(haystack.head) == Expr
+		populateexpression!(haystack.head, vals)
+	elseif typeof(haystack.head) == Symbol
+		if haskey(vals, string(haystack.head))
+			haystack.head = vals[string(haystack.head)]
+		end
+	end
+	for i = 1:length(haystack.args)
+		if typeof(haystack.args[i]) == Expr
+			populateexpression!(haystack.args[i], vals)
+		elseif typeof(haystack.args[i]) == Symbol
+			if haskey(vals, string(haystack.args[i]))
+				haystack.args[i] = vals[string(haystack.args[i])]
+			end
+		end
+	end
+end
+
 function replacesymbol!(haystack::Expr, needle::Symbol, replacement)
 	if typeof(haystack.head) == Expr
 		replacesymbol!(haystack.head, needle, replacement)
